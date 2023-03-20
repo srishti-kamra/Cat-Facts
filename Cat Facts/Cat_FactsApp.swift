bb8f11898251c037fd944f70b2f70998796bf366
Srishti Kamra <srishtikamra07@gmail.com>
2023-03-20 09:33:07 -0500

    Initial Commit

---
+++ Cat Facts/Cat_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_FactsApp.swift
+//  Cat Facts
+//
+//  Created by Srishti Kamra  on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Cat_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
